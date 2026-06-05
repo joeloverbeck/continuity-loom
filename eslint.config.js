3b76c0ac92c32befd644a9b0c68d63911c7e8520
joeloverbeck <joeloverbeck@gmail.com>
2026-06-05 12:25:09 +0200

    Implement SPEC002LOCPROFOL-006 requirements docs

--- eslint.config.js
+++ eslint.config.js
@@ -42,7 +42,8 @@ export default tseslint.config(
             "scripts/*.mjs",
             "packages/*/test/*.ts",
             "packages/*/src/*.test.ts"
-          ]
+          ],
+          maximumDefaultProjectFileMatchCount_THIS_WILL_SLOW_DOWN_LINTING: 32
         },
         tsconfigRootDir: import.meta.dirname
       }
